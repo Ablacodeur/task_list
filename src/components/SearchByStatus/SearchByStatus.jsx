2b78779002edbda38bd640edac186ef0c8504b84
Ablacodeur <agbolivicky@gmail.com>
2025-03-26 17:52:34 -0400

    add dot menu on small screen

--- src/components/SearchByStatus/SearchByStatus.jsx
+++ src/components/SearchByStatus/SearchByStatus.jsx
@@ -1,24 +1,69 @@
 import { useDispatch } from "react-redux";
-import s from "./style.module.css";
 import { settheStatus } from "../../store/task/userSearch-slice";
+import { MoreVert } from "@mui/icons-material";
+import { useState } from "react";
+import s from "./style.module.css"; 
+import { Typography } from "@mui/material";
+import { Button } from "@mui/base";
 
 export default function SearchByStatus() {
-  const status =["Completed",  "In Progress","Won't do"];
+  const status = ["Completed", "In Progress", "Won't do"];
+  const dispatch = useDispatch();
 
-  const  dispatch = useDispatch();
-
-  function onSelectArea(event){
-    dispatch(settheStatus(event.target.value))
+  function onSelectArea(event) {
+    dispatch(settheStatus(event.target.value));
   }
+
+  function resetStatus() {
+    dispatch(settheStatus(""));
+    setShowDropdown(false); 
+  }
+
+  const [showDropdown, setShowDropdown] = useState(false);
+
   return (
     <div>
-      <select className={s.input} onClick={''} onChange={onSelectArea}>
-        <option value="">Sort by: Status</option>
-        {status.map((theStatus,index)=>{
-          return(
-            <option value={theStatus} key={index}>{theStatus}</option>          )
-        })}
-      </select>
-  </div>
-  )
+      {/* Select pour les grands écrans */}
+      <div className={`${s.selectContainer}`}>
+        <select className={s.input} onChange={onSelectArea}>
+          <option value="">Sort by: Status</option>
+          {status.map((theStatus, index) => (
+            <option value={theStatus} key={index}>
+              {theStatus}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {/* Dropdown menu pour les petits écrans */}
+      <div className={`${s.searchStatus} ${showDropdown ? s.dropdown : ""}`}>
+        <button
+          className={s.menuButton}
+          onClick={() => setShowDropdown(!showDropdown)}
+        >
+          <MoreVert />
+        </button>
+        {showDropdown && (
+          <div className={s.dropdownMenu}>
+            <Typography
+              variant="h7"
+              onClick={resetStatus}
+              className={s.resetText} 
+            >
+              Sort by status
+            </Typography>
+            {status.map((theStatus, index) => (
+              <div
+                key={index}
+                className={s.menuItem}
+                onClick={() => onSelectArea({ target: { value: theStatus } })}
+              >
+                {theStatus}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
